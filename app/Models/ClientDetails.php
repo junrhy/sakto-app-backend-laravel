8684d6ebbced9e1c8bc46fde58c60ad837c7526a
Jun Rhy Crodua <jrcrodua@gmail.com>
2025-03-05 18:24:43 +0800

    add fillable

--- app/Models/ClientDetails.php
+++ app/Models/ClientDetails.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClientDetails extends Model
 {
-    //
+    protected $fillable = [
+        'client_id',
+        'app_name',
+        'name',
+        'value',
+        'client_identifier',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
 }
