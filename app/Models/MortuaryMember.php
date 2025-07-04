488002890cec230d3ca7084bff689f474cd23433
Jun Rhy Crodua <jrcrodua@gmail.com>
2025-07-05 00:58:50 +0800

    append

--- app/Models/MortuaryMember.php
+++ app/Models/MortuaryMember.php
@@ -30,6 +30,12 @@ class MortuaryMember extends Model
         'contribution_amount' => 'decimal:2'
     ];
 
+    protected $appends = [
+        'latest_contribution_amount',
+        'latest_contribution_date',
+        'total_contribution'
+    ];
+
     public function contributions()
     {
         return $this->hasMany(MortuaryContribution::class, 'member_id');
@@ -38,5 +44,22 @@ class MortuaryMember extends Model
     public function claims()
     {
         return $this->hasMany(MortuaryClaim::class, 'member_id');
+    }
+
+    public function getLatestContributionAmountAttribute()
+    {
+        $latestContribution = $this->contributions()->latest('payment_date')->first();
+        return $latestContribution ? $latestContribution->amount : null;
+    }
+
+    public function getLatestContributionDateAttribute()
+    {
+        $latestContribution = $this->contributions()->latest('payment_date')->first();
+        return $latestContribution ? $latestContribution->payment_date : null;
+    }
+
+    public function getTotalContributionAttribute()
+    {
+        return $this->contributions()->sum('amount');
     }
 } 
