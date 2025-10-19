517203c28fb7ecc29dfc8cb5ab03584cde6149ce
Jun Rhy Crodua <jrcrodua@gmail.com>
2025-10-19 09:59:22 +0800

    drop fnb_orders

--- database/migrations/2025_10_18_150138_create_fnb_orders_table.php
+++ database/migrations/2025_10_18_150138_create_fnb_orders_table.php
@@ -11,6 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Drop the table if it exists (safe since no data)
+        Schema::dropIfExists('fnb_orders');
+        
         Schema::create('fnb_orders', function (Blueprint $table) {
             $table->id();
             $table->string('client_identifier')->index();
